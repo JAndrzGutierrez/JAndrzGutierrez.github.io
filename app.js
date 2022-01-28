a468fdeb11852515c3910c2632c8a916d069581d
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-01-27 21:27:11 -0500

    file_update926pm

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 $(() => {
- 
-    const $navbutton = $(".navbutton");
-   
-  $navbutton.hover(
-      function () {
-        $(this).css("cursor", "pointer");
-      }
-      
-    );
+  // Navigation Bar
+  const $navbutton = $(".navbutton");
+
+  $navbutton.hover(function () {
+    $(this).css("cursor", "pointer");
   });
+
+  // $navbutton.on("click", function() {
+  //   window.open("https://www.youtube.com/watch?v=nhPaWIeULKk");
+  // });
+
+  $( ".navbutton" ).click(function() {
+    $( "#about_section" ).slideDown( "slow", function() {
+      // Animation complete.
+    });
+  });
+
+
+
+
+
+});
