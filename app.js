aaf7d6367920d1141fc182b6799976fe5198ad3a
Juan Andres <Juan@Devins-MacBook-Air.local>
2022-03-16 00:30:27 -0400

    updated file

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 $(() => {
-  // Navigation Bar
+  // Navigation Bar buttons
   const $navbutton = $(".navbutton");
 
   $navbutton.hover(function () {
     $(this).css("cursor", "pointer");
   });
 
-  $navbutton.on("click", function () {
-    window.open("https://www.youtube.com/watch?v=nhPaWIeULKk");
-  });
-
+  // $navbutton.on("click", function () {
+  //   window.open("");
+  // });
+// Bars Menu
   const $faFabars = $(".fa-bars");
 
   $faFabars.on("click", function () {
-    window.open("https://www.youtube.com/watch?v=vX9msKu75qs");
+    window.open("");
   });
+
+
 });
---
+++ images/Cooper's Closet_files/app.js
@@ -0,0 +1,26 @@
+// Wrap every letter in a span
+// const textWrapper = document.querySelector('.ml7 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml7 .letter',
+//     translateY: ["1.1em", 0],
+//     translateX: ["0.55em", 0],
+//     translateZ: 0,
+//     rotateZ: [180, 0],
+//     duration: 3000,
+//     easing: "easeOutExpo",
+//     delay: (el, i) => 50 * i
+//   }).add({
+//     targets: '.ml7',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
+
+anime({
+    targets: '.top-banner',
+    translateX:
+}) 
